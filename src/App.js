2299ae94c89158375ed0e4f57754302a633f4e45
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2022-09-05 10:46:55 +0100

    Color Generator

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+
+import Values from "values.js";
+
+function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#A4ed02").all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+      // console.log(colors);
+    } catch (error) {
+      setError(false);
+      // console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#A4ed02"
+            className={`${error ? "error" : null}`}
+          />
+
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+
+      <section className="colors">
+        {list.map((color, index) => {
+          // const hex = color.hex;
+          // console.log(color);
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+}
+
+export default App;
